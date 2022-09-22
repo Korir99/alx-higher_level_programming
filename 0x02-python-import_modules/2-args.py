9f4bb1bf36de52c7ca77c916c040a9307a5ce026
Korir99 <60397676+Korir99@users.noreply.github.com>
2022-09-22 23:51:39 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,15 @@
-‹filename:1:1: E902 FileNotFoundError: [Errno 2] No such file or directory: '‹filename'
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the number of and list of arguments."""
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
